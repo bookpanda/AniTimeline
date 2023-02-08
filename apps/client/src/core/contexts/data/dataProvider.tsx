4871c17298aa50c8e8afb8c90f78594dc0b88ce8
Idhibhat Pankam <marginpankam@gmail.com>
2023-02-08 23:38:10 +0700

    test: codegen

--- apps/client/src/core/contexts/data/dataProvider.tsx
+++ apps/client/src/core/contexts/data/dataProvider.tsx
@@ -1,12 +1,63 @@
 import { FC, PropsWithChildren, useEffect, useState } from "react";
 
+import { useQuery } from "@apollo/client";
+
 import { fetchData } from "$core/api/fetchData";
 import { initState } from "$core/api/initState";
 import { useAppContext } from "$core/contexts/app/appContext";
 
+import { graphql } from "../../../gql";
+
 import { DataContext } from "./dataContext";
 
+const allAnimeQueryDocument = graphql(/* GraphQL */ `
+  query ($username: String) {
+    MediaListCollection(userName: $username, type: ANIME, sort: SCORE_DESC) {
+      lists {
+        name
+        entries {
+          startedAt {
+            year
+            month
+            day
+          }
+          completedAt {
+            year
+            month
+            day
+          }
+          score
+          media {
+            characters {
+              nodes {
+                image {
+                  medium
+                }
+              }
+            }
+            title {
+              romaji
+              english
+              native
+              userPreferred
+            }
+            id
+            coverImage {
+              extraLarge
+              large
+              medium
+              color
+            }
+          }
+        }
+      }
+    }
+  }
+`);
 export const DataProvider: FC<PropsWithChildren> = ({ children }) => {
+  const variables = { username:"bookpanda" };
+  const { data } = useQuery(allAnimeQueryDocument, { variables })
+
   const appContext = useAppContext();
   const { sort, username } = appContext;
   const [data, setData] = useState(initState);
